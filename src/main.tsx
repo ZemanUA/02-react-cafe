93ab1a7ef91e661805c0308392028176e81a49c6
Valentyn Ziomenko <ziomenkow@gmail.com>
2026-05-28 22:28:08 +0200

    fix folders for components and their pathes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import { createRoot } from "react-dom/client";
-import App from "./components/App";
 import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
 
-createRoot(document.querySelector("#root") as HTMLDivElement).render(
-  <>
-    <StrictMode> 
-      <App></App>
-    </StrictMode>
-  </>
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
